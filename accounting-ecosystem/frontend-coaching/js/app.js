8254946a7bfc62a76b800ba2d2f3c60ade294500
Ruan van Loggerenberg <ruanvlog@lorenco.co.za>
2026-04-30 12:12:45 +0200

    Fix: Version clients.js dynamic import to bust browser cache

--- accounting-ecosystem/frontend-coaching/js/app.js
+++ accounting-ecosystem/frontend-coaching/js/app.js
@@ -156,7 +156,7 @@ function renderSidebarClients() {
                 li.classList.add('active');
                 
                 // Open client
-                import('./clients.js').then(module => {
+                import('./clients.js?v=2').then(module => {
                     module.openClient(client.id);
                 });
             });
